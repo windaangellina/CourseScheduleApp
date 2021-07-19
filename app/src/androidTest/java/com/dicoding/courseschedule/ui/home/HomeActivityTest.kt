12b83daaecc95cc0d3a54b86b660cc4d0281b61b
Winda Angelina Utama <wau959@gmail.com>
2021-07-19 16:25:46 +0700

    beres testing. tips testing : run > record espresso test

---
+++ app/src/androidTest/java/com/dicoding/courseschedule/ui/home/HomeActivityTest.kt
@@ -0,0 +1,67 @@
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import com.dicoding.courseschedule.R
+import com.dicoding.courseschedule.ui.add.AddActivity
+import com.dicoding.courseschedule.ui.home.HomeActivity
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+class HomeActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(HomeActivity::class.java)
+
+    @Test
+    fun homeActivityTest() {
+        val actionMenuItemView = onView(
+            allOf(withId(R.id.action_add), withContentDescription("Add Course"),
+            childAtPosition(
+                childAtPosition(withId(R.id.action_bar), 1),
+                0
+            ),
+            isDisplayed()))
+
+        Intents.init()
+        actionMenuItemView.perform(click())
+        Intents.intended(IntentMatchers.hasComponent(AddActivity::class.java.name))
+        Intents.release()
+    }
+    
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
